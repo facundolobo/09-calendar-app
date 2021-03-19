bc163a155f34c33ed0d45b78eacccc839a280df9
Facundo Lobo <facundolobo2@gmail.com>
2021-03-19 09:49:27 -0300

    7. Configuraciones adicionales al calendario

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,19 +1,42 @@
-import React from 'react'
-import { Navbar } from '../ui/Navbar'
+import React from 'react';
+import { Navbar } from '../ui/Navbar';
 //calendario
     import 'react-big-calendar/lib/css/react-big-calendar.css' //css para que se vea bien
-    import {Calendar, momentLocalizer} from 'react-big-calendar'
-    import moment from 'moment'
+    import {Calendar, momentLocalizer} from 'react-big-calendar';
+    import moment from 'moment';
+    import { messages } from '../helpers/calendar-messages-es'; //cambio de idioma
+
+    import 'moment/locale/es';
+    moment.locale('es') //cambiar el idioma a moment
+    
     const localizer = momentLocalizer(moment) // or globalizeLocalizer
     //creamos un evento apra enviar al calendario
     const event = [{
         title: 'Cumpleaños del jefe',
         start: moment().toDate(), //es como new Date() // inicio del evento
-        end: moment().add(2, 'hours').toDate()  //fin del evento mas 2 horas
+        end: moment().add(2, 'hours').toDate(),  //fin del evento mas 2 horas
+        bgcolor: '#fafafa',
+        notes: 'comprar el pastel'
     }]
-//-
+    //-
+    
 
 export const CalendarScreen = () => {
+    
+    const eventStyleGetter =(event, start, end, isSelected)=>{
+        //console.log(event, start, end, isSelected)
+        const style = {
+            backgroundColor: '#367CF7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            display: 'block',
+            color: 'white'
+        }
+        return{
+            style
+        }
+    };
+    
     return (
         <div className="calendar-screen">
             <Navbar/>
@@ -23,6 +46,8 @@ export const CalendarScreen = () => {
                 events={event}
                 startAccessor="start"
                 endAccessor="end"
+                messages={messages} //enviamos el cambio de idioma
+                eventPropGetter={eventStyleGetter}
             />  
         </div>
     )
